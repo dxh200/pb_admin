e5133f7f124586e64fb3a1a7786a069f04303cc4
dxh <xiao00hei@163.com>
2018-07-06 18:39:09 +0800

    dxh

---
+++ utils/ResultAjax.js
@@ -0,0 +1,65 @@
+/**
+ * 返回ajax 信息
+ */
+class ResultAjax{
+    constructor(code,msg,data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    setCode(code){
+        this.code = code;
+        return this;
+    }
+    getCode(){
+        return this.code;
+    }
+    setMsg(msg){
+        this.msg = msg;
+        return this;
+    }
+    getMsg(){
+        return this.msg;
+    }
+    setData(data){
+        this.data = data;
+        return this;
+    }
+    getData() {
+        return this.data;
+    }
+
+    getRes(){
+        return {"code":this.code,"msg":this.msg,"data":this.data};
+    }
+}
+module.exports = {
+    SUCCESS:(msg,data)=>{
+        if(!msg){
+            msg = "success";
+        }
+        if(!data){
+            data = {};
+        }
+        return new ResultAjax("0",msg,data);
+    },
+    FAILED:(msg,data)=>{
+        if(!msg){
+            msg = "failed";
+        }
+        if(!data){
+            data = {};
+        }
+        return new ResultAjax("1",msg,data);
+    },
+    ERROR:(msg,data)=>{
+        if(!msg){
+            msg = "error";
+        }
+        if(!data){
+            data = {};
+        }
+        return new ResultAjax("-1",msg,data);
+    }
+};
